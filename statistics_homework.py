6fb13ff1487bf3beb3ca186136001ce6dae4df81
piezga <piezga@gmail.com>
2021-03-23 15:50:00 +0100

    added the statistics exercise

---
+++ statistics_homework.py
@@ -0,0 +1,25 @@
+from __future__ import print_function, division
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+
+
+maxSize = 300
+
+sizes = np.arange(1,maxSize)
+
+means = np.array([])
+for i in range(1,maxSize):
+    numbers = stats.poisson.rvs(mu = 8, size = i*10)
+    mean = numbers.mean()
+    means = np.append(means,mean)
+    
+print(means)
+
+
+plt.scatter(sizes,means)
+plt.title('I used the poisson distribution')
+plt.xlabel('Sample size/10')
+plt.ylabel('Calculated mean')
+plt.axhline(8, color ='r')
+plt.show()
