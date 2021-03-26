39550e138e31a9b48c1133f3a809302380de66a2
piezga <piezga@gmail.com>
2021-03-26 21:40:59 +0100

    organized folders

--- untitled0.py
+++ untitled0.py
@@ -1,15 +1,23 @@
-import pandas as pd
+import scipy.stats as sps
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
-df = pd.read_csv("https://raw.githubusercontent.com/fedhere/DSPS/master/labs/1865331.csv")
+eqdata = pd.read_csv("https://raw.githubusercontent.com/fedhere/MLPNS2021/main/NHRT/earthquakes.csv", sep=" ")
 
-listnumv = ["PRCP", "SNOW", "SNWD", "WESD","WES"]
+#rename columns
 
+eqdata.rename({"#YYY/MM/DD" : "date",
+               "Unnamed: 5" : "mag",
+               "HH:mm:SS.ss": "time"},axis=1)[["date","time","mag"]]
 
-samp = df.DATE[pd.to_datetime(df.DATE) > pd.to_datetime("2018-12-31")]
-pop = df.DATE[~(pd.to_datetime(df.DATE) > pd.to_datetime("2018-12-31"))]
+#data['HH:mm:SS.ss'] = data['HH:mm:SS.ss'].str.replace('60.00', '59.99')
 
-def Z(pop,samp):
-    return (pop.mean() - samp.mean())/(np.stdev(pop)) / np.sqrt(len(samp))
+for i in range(len(eqdata["time"])):
+    if eqdata.iloc[i].time.endswith("60.00"):
+        print(eqdata.iloc[i].time)
+        print(eqdata.iloc[i].time[:6] + '59.99')
+        eqdata["time"][i] = eqdata.iloc[i].time[:6] + "59.99"
+        
 
-print(Z(pop.PRCP, samp.PRCP))
+pd.to_datetime(eqdata['time'])
